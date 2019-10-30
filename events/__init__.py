506bc56ba9a3e4391458e886be8c43b40b60d094
ZephyrBlu <lukejholroyd@gmail.com>
2019-10-31 10:38:36 +1300

    Fix exports in events module

--- events/__init__.py
+++ events/__init__.py
@@ -1,10 +1,19 @@
+from zephyrus_sc2_parser.events.object_event import ObjectEvent
+from zephyrus_sc2_parser.events.ability_event import AbilityEvent
+from zephyrus_sc2_parser.events.selection_event import SelectionEvent
+from zephyrus_sc2_parser.events.control_group_event import ControlGroupEvent
+from zephyrus_sc2_parser.events.upgrade_event import UpgradeEvent
+from zephyrus_sc2_parser.events.camera_update_event import CameraUpdateEvent
+from zephyrus_sc2_parser.events.player_stats_event import PlayerStatsEvent
+from zephyrus_sc2_parser.events.player_leave_event import PlayerLeaveEvent
+
 __all__ = [
-    "object_event",
-    "ability_event",
-    "selection_event",
-    "control_group_event",
-    "upgrade_event",
-    "camera_update_event",
-    "player_stats_event",
-    "player_leave_event",
+    "ObjectEvent",
+    "AbilityEvent",
+    "SelectionEvent",
+    "ControlGroupEvent",
+    "UpgradeEvent",
+    "CameraUpdateEvent",
+    "PlayerStatsEvent",
+    "PlayerLeaveEvent",
 ]
