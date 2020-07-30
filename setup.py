40789436ecf68de8c2a61faeb2e6de4e0e600bcd
ZephyrBlu <lukejholroyd@gmail.com>
2020-07-31 00:55:22 +1200

    Add protocol 81009 for Patch 5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="zephyrus-sc2-parser",
-    version="0.2.23",
+    version="0.2.24",
     author="Luke Holroyd",
     author_email="hello@zephyrus.gg",
     description="Parser for SC2 replay files",
