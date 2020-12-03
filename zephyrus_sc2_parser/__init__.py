b98ff9cfe72c45aac844127e1edc3d361b16889f
ZephyrBlu <lukejholroyd@gmail.com>
2020-12-03 13:20:50 +1300

    Supress unknown event warnings from util logger

--- zephyrus_sc2_parser/__init__.py
+++ zephyrus_sc2_parser/__init__.py
@@ -3,3 +3,7 @@ from logging import NullHandler
 from zephyrus_sc2_parser.parser import parse_replay
 
 logging.getLogger(__name__).addHandler(NullHandler())
+
+# to prevent unknown event warnings
+util_logger = logging.getLogger(f'{__name__}.utils')
+util_logger.setLevel(logging.ERROR)
