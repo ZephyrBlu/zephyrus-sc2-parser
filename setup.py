ed9ffd7c4f074ca6a2443560e7917451db7b80d7
Moritz Bracht <682686+dermorz@users.noreply.github.com>
2022-03-07 13:30:31 +0100

    Update parser for 5.0.8, protocol 86383 (#24)
    * Update parser for patch 5.0.8, protocol 86383
    
    * Bump version
    
    * Regenerate new gamadata with generate_gamedata.py
    
    * Update base unit costs + regenerate unit data
    
    Co-authored-by: ZephyrBlu <lukejholroyd@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="zephyrus-sc2-parser",
-    version="0.3.5",
+    version="0.3.6",
     author="Luke Holroyd",
     author_email="hello@zephyrus.gg",
     description="Parser for SC2 replay files",
